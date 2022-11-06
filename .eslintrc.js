f389c5b6b9f9f1c62b2c31dac56ede70fc231c7b
flex2020 <hosan_1@naver.com>
2022-11-06 17:39:05 +0900

    eslint 수정
    eslint 수정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 0,
+  },
+};
