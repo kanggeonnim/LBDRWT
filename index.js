24a407cb8a05799a78151991714e0814b06f1243
flex2020 <hosan_1@naver.com>
2022-11-17 13:58:09 +0900

    Update Index.js for the Issue #1

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
 const { RTMClient } = require('@slack/rtm-api');
-
-const tokenModule = require('./data');
+const tokenModule = require('./data/token');
 
 const token = tokenModule.getToken();
-console.log(token);
+console.log('[Chatbot Start] : ', token);
 
 const rtm = new RTMClient(token);
 rtm.start();
-// const token = t.token;
-const greeting = require('./greeting');
-const square = require('./square');
+
+const greeting = require('./greeting/greeting');
+// const greetingTest = require('./greeting/greetingTest');
+const square = require('./square/square');
 
 rtm.on('message', (message) => {
   const { channel } = message;
@@ -22,7 +22,12 @@ rtm.on('message', (message) => {
   } else {
     switch (text) {
       case 'hi':
-        greeting(rtm, channel);
+        greeting(rtm, channel); // 확률 테스트 시 이 부분을 주석처리 후 테스트
+
+        // 실제 테스트 시 for문을 주석처리 후 테스트
+        // for (let i = 0; i < 100000; i += 1) {
+        //   greetingTest();
+        // }
         break;
       default:
         rtm.sendMessage("I'm alive", channel);
