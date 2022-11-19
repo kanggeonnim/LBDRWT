35ae2edb6a7291b247e36b1764f2ee75b12e0d85
dpdltm2944 <102200639+dpdltm2944@users.noreply.github.com>
2022-11-20 07:33:53 +0900

    Update index.js for the issue #11

--- index.js
+++ index.js
@@ -5,50 +5,38 @@ const tokenModule = require('./data/token');
 
 const token = tokenModule.getToken();
 console.log(token);
-
 const rtm = new RTMClient(token);
 rtm.start();
 // const token = t.token;
 const greeting = require('./greeting/greeting');
 const square = require('./square/square');
+const getOffice = require('./office/getoffice');
 
 rtm.on('message', (message) => {
   const { channel } = message;
   const { text } = message;
-
   if (Number(text)) {
     square(rtm, text, channel);
+  } else if (text.includes('교수님')) {
+    const name = text.substring(0, text.length - 4);
+    getOffice(rtm, name, channel);
   } else {
     switch (text) {
       case 'hi':
         greeting(rtm, channel);
         break;
+      case 'quit':
+        rtm.sendMessage('Bye!', channel);
+        rtm.disconnect();
+        break;
+      /* case '김성찬':
+      // checkOffice(text)의 반환값을 콘솔로그로 표시
+        checkOffice(text);
+        getOffice(rtm, text, channel);
+        break; */
       default:
         rtm.sendMessage("I'm alive", channel);
-    }
+        break;
+    } // text에 사무실이 포함되어 있으면
   }
 });
-/**
- * 1. 토큰을 가져온다.
- * 2. RTMClient를 생성한다.
- * 3. RTMClient를 시작한다.
- * 4. RTMClient가 메시지를 받으면, 메시지를 처리한다.
- * 5. 메시지를 처리하는 방법은 다음과 같다.
- * 5-1. 메시지가 숫자이면, square.js를 실행한다.
- * 5-2. 메시지가 'hi'이면, greeting.js를 실행한다.
- * 5-3. 메시지가 'hi'가 아니면, 'I'm alive'라고 응답한다.
- * 6. square.js는 다음과 같이 동작한다.
- * 6-1. 메시지를 받는다.
- * 6-2. 메시지를 숫자로 변환한다.
- * 6-3. 숫자를 제곱한다.
- * 6-4. 제곱한 숫자를 채널에 보낸다.
- * 7. greeting.js는 다음과 같이 동작한다.
- * 7-1. 메시지를 받는다.
- * 7-2. 채널을 받는다.
- * 7-3. 'Hello!'라고 채널에 보낸다.
- * 8. 모든 모듈은 다음과 같이 동작한다.
- * 8-1. 메시지를 받는다.
- * 8-2. 채널을 받는다.
- * 8-3. 메시지를 처리한다.
- * 8-4. 처리한 결과를 채널에 보낸다.
- */
