114795ca910be1ec6c3d3888fc924f9a83b8e249
dpdltm2944 <102200639+dpdltm2944@users.noreply.github.com>
2022-11-20 07:41:00 +0900

    Create getoffice.js for the Issue #11

--- office/getoffice.js
+++ office/getoffice.js
@@ -1,3 +1,19 @@
+/**
+ * 설명
+ * @param {string} rtm - token
+ * @param {string} name - 교수님 이름
+ * @param {string} office - 교수님 사무실
+ * @param {string} channel - 채널
+ * @return {string} - 교수님 사무실
+ * 루틴 설명
+ * 1. office.json 파일을 읽어옴
+ * 2. office.json 파일을 파싱
+ * 3. 파싱한 데이터에서 name을 키로 가지는 데이터를 찾음
+ * 4. 찾은 데이터를 반환
+ * 5. 찾지 못하면 undefined를 반환
+ * 6. 반환된 데이터를 rtm.sendMessage로 보냄
+ * 7. 보낸 데이터를 콘솔에 출력
+ */
 const fs = require('fs');
 
 const getOffice = function (rtm, name, channel) {
@@ -14,10 +30,3 @@ const getOffice = function (rtm, name, channel) {
 };
 
 module.exports = getOffice;
-/**
- * 1. fs.readFile을 통해 office.json을 읽어옴
- * 2. JSON.parse를 통해 json을 파싱
- * 3. 파싱한 json에서 name을 통해 office를 찾아냄
- * 4. rtm.sendMessage를 통해 메시지를 보냄
- * 5. true를 반환
- */
