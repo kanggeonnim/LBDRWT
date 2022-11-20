1709f180a67aacd41b9ede380034550ab0abd950
flex2020 <hosan_1@naver.com>
2022-11-20 16:50:37 +0900

    Update greeting.spec.js (assert 사용)

--- test/greeting.spec.js
+++ test/greeting.spec.js
@@ -2,6 +2,7 @@
 /**
  * 이 모듈은 greeting 모듈을 테스트하기 위한 테스팅용 모듈입니다.
  */
+const assert = require('assert');
 const randomGreeting = require('../greeting/randomGreeting');
 const statistics = require('../utils/statistics');
 
@@ -13,8 +14,8 @@ const greetings = randomGreeting.getGreetings();
 * testingCounts만큼 랜덤한 결과가 나오게 되면 성공입니다.
 */
 
-describe('Random Greeting Test Start!', () => {
-  it('Random Greeting Test End!', (done) => {
+describe('====Random Greeting Test Start!====', () => {
+  it('Random Greeting Test End!======', (done) => {
     let count = 0;
     const testingCounts = 10000; // 테스트 횟수를 정한 후 시작
     for (let i = 0; i < testingCounts; i += 1) {
@@ -24,10 +25,9 @@ describe('Random Greeting Test Start!', () => {
       }
     }
     // 테스트가 성공적으로 끝난 경우
-    if (count === testingCounts) {
-      console.log(`${testingCounts}회 테스트 성공`);
-      statistics.displayPercentages();
-      done();
-    }
+    assert.equal(count, testingCounts);
+    console.log(`${testingCounts}회 테스트 성공`);
+    statistics.displayPercentages();
+    done();
   });
 });
