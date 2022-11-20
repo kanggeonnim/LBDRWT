3a0b3a41742e09196f6dbb662d9c39b34186fe0f
flex2020 <hosan_1@naver.com>
2022-11-20 16:51:24 +0900

    Create square.spec.js for the mocha

---
+++ test/square.spec.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const assert = require('assert');
+const square = require('../square/squareCalculation'); // 제곱 계산 모듈을 불러옴.
+
+describe('====Square Test Start!====', () => {
+  it('Square Test End!======', (done) => {
+    // square 모듈을 사용한 값과 실제 제곱의 값을 비교함.
+    // 테스트 횟수동안 항상 같은 값을 뱉어내면 성공
+    const testingCounts = 10000;
+    let count = 0;
+
+    for (let i = 1; i <= testingCounts; i += 1) {
+      const squareNumber = i * i;
+      const squareNumber2 = square(i);
+      if (squareNumber === squareNumber2) {
+        count += 1;
+      }
+    }
+    // 테스트가 성공적으로 끝난 경우
+    assert.equal(count, testingCounts);
+    console.log(`${testingCounts}회 테스트 성공`);
+    done();
+  });
+});
