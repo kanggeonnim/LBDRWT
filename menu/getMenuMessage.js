b3c7954636118e792e7aa6f87354448837f99d4b
NOH UIBIN <hosan_1@naver.com>
2022-12-04 16:35:19 +0900

    Update getMenuMessage.js - 평가 모듈 합침

--- menu/getMenuMessage.js
+++ menu/getMenuMessage.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-restricted-syntax */
 const webScraping = require('../utils/webScraping');
+const getRating = require('./getRating');
 
 const url = 'https://sobi.chonbuk.ac.kr/menu/week_menu.php';
 const baseSelector = 'div.section div.ttArea:nth-child(2) table.tblType03 tbody tr:first-child ';
@@ -11,7 +12,7 @@ const getMenuMessage = async function () {
     let message = '';
     const selector = (param === 3) ? `${baseSelector}td:nth-child(${param}) ul li > span` : `${baseSelector}td:nth-child(${param}) ul li > font`;
     const result = await webScraping(url, selector);
-    const score = 2; // 수정 예정
+    const score = getRating(result[0]);
     message = `${result}(으)로 별점 ${score}점입니다.`;
     messages.push(message);
   }
