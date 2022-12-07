3282969e1fb268c6366ce639744694cd14e7d6ee
kanggeonnim <gun4329@gmail.com>
2022-12-07 16:40:46 +0900

    update: add comment - 1-7line

--- menu/sendWeekMenu.js
+++ menu/sendWeekMenu.js
@@ -1,7 +1,9 @@
 /**
- * 오늘 메뉴에 대한 메시지를 전송하는 함수입니다.
+ * 이번주 메뉴에 대한 메시지를 전송하는 함수입니다.
  * @param {*} rtm // 슬랙의 real-time-messaging client
  * @param {*} channel // 현재 연결된 채널
+ * @param {*} menuData 한주의 메뉴가 담긴 데이터
+ * @param {*} menuFlag 메뉴가 로딩되었는지 알려주는 값
  */
 const days = ['월요일', '화요일', '수요일', '목요일', '금요일'];
 const sendWeekMenuMessage = function (rtm, channel, menuData, menuFlag) {
