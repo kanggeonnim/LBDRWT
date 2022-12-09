42c9ac3b67269b98475dbfd87ebabb39a3a67dc0
dpdltm2944 <102200639+dpdltm2944@users.noreply.github.com>
2022-12-09 19:14:44 +0900

    update: getOffice.sepc.js for unit test

--- test/getOffice.spec.js
+++ test/getOffice.spec.js
@@ -4,18 +4,25 @@ const assert = require('assert');
 
 const getOffice = require('../office/getOffice');
 const getData = require('../utils/getData');
+const getWrongData = require('../utils/getWrongData');
+const getRandomIndex = require('../utils/randomIndexGenerator');
+const forIndex = require('../utils/getLineCount');
 
+const num = forIndex();
 describe('===getOffice test start', () => {
   it('getOffice test End===', (done) => {
-    let count = 10;
-    const testingCount = 10;
+    let count = 3;
+    const testingCount = 3;
     for (let i = 0; i < testingCount; i += 1) {
-      const num = getData(i);
-      // console.log(num);
-      const location = getOffice(num);
+      // console.log(i);
+      const name = getData(getRandomIndex(num));
+      testName = getWrongData(name);
+      // console.log(name, 'spec num');
+      // console.log(testName, 'spec testName');
+      const location = getOffice(testName);
       // console.log(location);
       if (location === null) { break; }
-      if (location.toString().toLowerCase().replace(/\s/g, '').includes(num.replace(/\s/g, '').toString().toLowerCase())) {
+      if (location.toString().includes(name)) {
         count -= 1;
         // console.log(count);
       }
